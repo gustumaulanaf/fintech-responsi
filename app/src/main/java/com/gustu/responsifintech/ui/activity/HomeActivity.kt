51162a5a134e99fcd52a9df6630923e47ed0d13a
gustumaulanaf <gustumaulanaf@gmail.com>
2021-07-07 00:15:55 +0700

    setup home fragment

---
+++ app/src/main/java/com/gustu/responsifintech/ui/activity/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.gustu.responsifintech.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gustu.responsifintech.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
